f0b638a7afca5086d6f369ea4069948eaad5074d
Maxxiandrade <maxiandrade62@gmail.com>
2023-11-22 18:11:07 -0300

    Register by email/password

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,18 +1,45 @@
-
+import { useState } from "react";
+import {auth} from '../../firebase-config'
+import { createUserWithEmailAndPassword } from "firebase/auth";
 const Register = ()=>{
-    return(
-        <form action="">
-                <label htmlFor="">Email</label>
-                <br />
-                <input type="text" name="" id="" />
-                <hr />
-                <label htmlFor="">Password</label>
-                <br />
-                <input type="password" />
-                <br />
-                <button>Register</button>
-            </form> 
-    )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async(event)=>{
+        event.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log('Usuario registrado:', user);
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+    return (
+        <form onSubmit={handleRegister}>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          <hr />
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      );
 };
 
 export default Register
