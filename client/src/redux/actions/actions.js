306915ffbb7938020687d418625dbe4f45088fbd
Jose I. Martinez <joseignacio2534@gmail.com>
2023-11-24 18:22:24 -0300

    action types actualizada

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { SET_USER_DATA_REGISTER, SET_USER_DATA_CREATE_PROFILE, CREATE_NEW_USER } from "../action_types/userActionTypes";
+import {
+  SET_USER_DATA_REGISTER,
+  SET_USER_DATA_CREATE_PROFILE,
+  CREATE_NEW_USER,
+  GET_ALL_USERS,
+} from "../action_types/userActionTypes";
 import { GET_ONLINE } from "../action_types/action-types";
-
-export const GET_ALL_USERS = "GET_ALL_USERS";
-export const ERROR_GETTING_USERS = "ERROR_GETTING_USERS";
-
 
 export const getAllUsers = () => {
   return async function (dispatch) {
@@ -15,12 +16,9 @@ export const getAllUsers = () => {
       if (response.data.success) {
         const users = response.data.users;
         dispatch({ type: GET_ALL_USERS, payload: users });
-      } else {
-        // Enviar un mensaje de error si "success" es false
-        throw new Error("Error fetching users");
       }
     } catch (error) {
-      dispatch({ type: ERROR_GETTING_USERS, payload: error.message });
+      throw Error(error);
     }
   };
 };
@@ -30,51 +28,53 @@ export const setUserDataRegister = (userData) => {
     try {
       dispatch({
         type: SET_USER_DATA_REGISTER,
-        payload: userData
+        payload: userData,
       });
     } catch (error) {
-      throw Error(error)
+      throw Error(error);
     }
-  }
-}
+  };
+};
 
 export const setUserDataCreateProfile = (userData) => {
   return async function (dispatch) {
     try {
       dispatch({
         type: SET_USER_DATA_CREATE_PROFILE,
-        payload: userData
+        payload: userData,
       });
       console.log(userData);
     } catch (error) {
-      throw Error(error)
+      throw Error(error);
     }
-  }
-}
+  };
+};
 
 export const createNewUser = (userData) => {
   return async function (dispatch) {
     try {
-      const response = await axios.post("http://localhost:3001/createuser", userData);
+      const response = await axios.post(
+        "http://localhost:3001/createuser",
+        userData
+      );
       dispatch({
         type: CREATE_NEW_USER,
-        payload: response
-      })
+        payload: response,
+      });
       console.log(response);
     } catch (error) {
-      throw Error(error)
+      throw Error(error);
     }
-  }
-}
+  };
+};
 
-export const getOnline = ()=>async(dispatch)=>{
-    try {
-      const {data} = await axios.get('http://localhost:3001/getonline')
-      if(data){
-        dispatch({type: GET_ONLINE, payload: data})
-      }
-    } catch (error) {
-      throw Error(error)
+export const getOnline = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get("http://localhost:3001/getonline");
+    if (data) {
+      dispatch({ type: GET_ONLINE, payload: data });
     }
+  } catch (error) {
+    throw Error(error);
   }
-
+};
